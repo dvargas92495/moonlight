d0005368daf404f2cb5980b6b176f6e3f56ca138
unknown <dvargas92495@gmail.com>
2019-12-10 00:13:49 -0500

    Downloading aws cli

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,14 +22,13 @@ const AppContainer = styled.div`
 `;
 
 const AppHeader = styled.header`
-  background-color: #282c34;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: calc(10px + 2vmin);
-  color: white;
+  color: #282c34;
 `;
 
 const MainHeader = styled.p`
