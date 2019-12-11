89a7a7df8ac89ebb5c93014ec809190b91f1bb3a
unknown <dvargas92495@gmail.com>
2019-12-10 22:55:30 -0500

    Deploy and Install scripts

--- travis/after_deploy.sh
+++ travis/after_deploy.sh
@@ -1,7 +1,2 @@
-curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-export AWS_CONFIG_FILE=.awsconfig
-
 aws cloudfront create-invalidation --distribution-id E1RUJ2RP8VS0PX --paths "/*"
 aws cloudfront create-invalidation --distribution-id E2E35W4UJ7YKON --paths "/*"
---
+++ travis/deploy.sh
@@ -0,0 +1,1 @@
+aws s3 sync client/build s3://moonlight-client
