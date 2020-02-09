8123445fe44036a334f4201e637ef51ee13c2806
David Vargas <dvargas92495@gmail.com>
2020-02-08 23:32:23 -0500

    Installing terraform (#15)

--- travis/install.sh
+++ travis/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 pip install awscli --upgrade --user
+
+curl -sLo ./terraform.zip https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip
+unzip ./terraform.zip -d ./
+rm terraform.zip
