a96f41c357d4e2168cf1a4439ba31771465f78f5
David Vargas <dvargas92495@gmail.com>
2020-02-10 23:25:15 -0500

    Starting to port deploy logic to one script (#18)

--- travis/script-branch.sh
+++ travis/script-branch.sh
@@ -3,54 +3,12 @@
 cd terraform
 ./terraform init
 ./terraform apply -auto-approve
-
 cd ..
-echo "
-REACT_APP_AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-REACT_APP_USER_CLIENT_SECRET=${REACT_APP_USER_CLIENT_SECRET}
-REACT_APP_RDS_MASTER_USER_PASSWORD=${RDS_MASTER_USER_PASSWORD}
-" > client/.env.local
 
-cd db
-# npm install
-# npm run migrate
+DOMAIN="env1.qa.moonlight-health.com"
 
-cd ../client
+./script.sh $TF_WORKSPACE $DOMAIN
+
+export CYPRESS_BASE_URL="https://$DOMAIN"
+cd qa
 npm test
-npm run build
-aws s3 sync --delete build "s3://${TF_WORKSPACE}"
-
-CLOUDFRONT_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{Alias:Aliases.Items[0],Id:Id}[?Alias=='env1.qa.moonlight-health.com'].Id" --output text)
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths "/*"
-
-# ROUTE53_CHANGE_ID=$(aws route53 change-resource-record-sets --hosted-zone-id Z18VX8M08PX0TW --query "ChangeInfo.Id" --output text --change-batch "{
-#   \"Comment\": \"Creating Alias resource record set in Route 53 for ${DOMAIN}\",
-#   \"Changes\": [{
-#     \"Action\": \"CREATE\",
-#     \"ResourceRecordSet\": {
-#       \"Name\": \"${DOMAIN}\",
-#       \"Type\": \"A\",
-#       \"AliasTarget\":{
-#         \"HostedZoneId\": \"Z3AQBSTGFYJSTF\",
-#         \"DNSName\": \"s3-website-us-east-1.amazonaws.com\",
-#         \"EvaluateTargetHealth\": false
-#       }
-#     }
-#  }]
-# }")
-
-# NEXT_WAIT_TIME=0
-# OUTPUT="PENDING"
-# until [[ "$OUTPUT" = INSYNC* ]]; do
-#   sleep $NEXT_WAIT_TIME
-#   OUTPUT=$(aws route53 get-change --id "/change/${ROUTE53_CHANGE_ID}" --query "ChangeInfo.Status" --output text)
-#   let NEXT_WAIT_TIME=NEXT_WAIT_TIME+1
-#   echo "After ${NEXT_WAIT_TIME} attempt, ID ${ROUTE53_CHANGE_ID} has status ${OUTPUT}"
-#   if [ $NEXT_WAIT_TIME -eq 20 ]; then
-#     exit 1
-#   fi  
-# done
-
-# export CYPRESS_BASE_URL="http://$DOMAIN"
-# cd ../qa
-# npm test
--- travis/script-master.sh
+++ travis/script-master.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-echo "
-REACT_APP_AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-REACT_APP_USER_CLIENT_SECRET=${REACT_APP_USER_CLIENT_SECRET}
-REACT_APP_RDS_MASTER_USER_PASSWORD=${RDS_MASTER_USER_PASSWORD}
-" > client/.env.local
-
-cd db
-npm install
-npm run migrate
+./script.sh moonlight-client moonlight-health.com
 
 cd ../lambda
 npm install
@@ -49,11 +41,4 @@ aws lambda update-function-code \
     --publish \
     --zip-file fileb://getSpecialistViews.zip \
 
-cd ../client
-npm install
-npm run build
-
-aws s3 sync --delete build s3://moonlight-client
-
-aws cloudfront create-invalidation --distribution-id E1RUJ2RP8VS0PX --paths "/*"
 aws cloudfront create-invalidation --distribution-id E2E35W4UJ7YKON --paths "/*"
