3d524cd213b78743e27e0dca66f65e8a4c1663e4
unknown <dvargas92495@gmail.com>
2020-02-15 13:35:35 -0500

    I hate bash

--- travis/script-master.sh
+++ travis/script-master.sh
@@ -6,22 +6,18 @@ cd lambda
 npm install
 npm run build
 
-update-function() {
-    zip -jq $1.zip ./build/$1.js
-    aws lambda update-function-code \
-        --function-name "moonlight-health-$1" \
-        --publish \
-        --zip-file fileb://$1.zip \
+update_function () {
+    aws lambda update-function-code --function-name "moonlight-health-${1}" --publish --zip-file "fileb://${1}.zip"
 }
 
-update-function signUp
-update-function confirmSignUp
-update-function signIn
-update-function getAvailability
-update-function putAvailability
-update-function getProfile
-update-function putProfile
-update-function getSpecialistViews
-update-function postEvents
+update_function signUp
+update_function confirmSignUp
+update_function signIn
+update_function getAvailability
+update_function putAvailability
+update_function getProfile
+update_function putProfile
+update_function getSpecialistViews
+update_function postEvents
 
 aws cloudfront create-invalidation --distribution-id E2E35W4UJ7YKON --paths "/*"
