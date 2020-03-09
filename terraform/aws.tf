1c7cb74674e4deb7f4698331205c6dd30906082f
David Vargas <dvargas92495@gmail.com>
2020-03-08 21:59:30 -0400

    Terraforming the www subdomain (#36)
    * Terraforming the www subdomain
    
    * r53 domains
    
    * custom origin config

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -58,6 +58,18 @@ POLICY
   }
 }
 
+resource "aws_s3_bucket" "www" {
+  bucket = "www-${local.env_name}"
+  acl    = "private"
+  tags = {
+    Application = "Moonlight"
+  }
+
+  website {
+    redirect_all_requests_to = "https://moonlight-health.com"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "client" {
   bucket = aws_s3_bucket.client.id
 
@@ -65,6 +77,15 @@ resource "aws_s3_bucket_public_access_block" "client" {
   block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_public_access_block" "www" {
+  bucket = aws_s3_bucket.www.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_acm_certificate" "cert" {
@@ -196,6 +217,99 @@ resource "aws_route53_record" "AAAA" {
   alias {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_cloudfront_distribution" "s3_www_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.www.website_endpoint
+    origin_id   = "www-${local.s3_origin_id}"
+    
+    custom_origin_config {
+      http_port    = "80"
+      https_port   = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = ""
+
+  aliases = ["www.${local.domain}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "www-${local.s3_origin_id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  tags = {
+    Application = "Moonlight"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+}
+
+resource "aws_route53_record" "www-A" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "www.${local.domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_www_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_www_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www-AAAA" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "www.${local.domain}"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_www_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_www_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
--- travis/script.sh
+++ travis/script.sh
@@ -44,3 +44,6 @@ aws s3 sync --delete build "s3://${ENV_NAME}"
 
 CLOUDFRONT_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{Alias:Aliases.Items[0],Id:Id}[?Alias=='${DOMAIN}'].Id" --output text)
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths "/*"
+
+WWW_CLOUDFRONT_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{Alias:Aliases.Items[0],Id:Id}[?Alias=='www.${DOMAIN}'].Id" --output text)
+aws cloudfront create-invalidation --distribution-id $WWW_CLOUDFRONT_ID --paths "/*"
