c9ea56858b00a8c3600d15ef12d20f28afb7d0de
unknown <dvargas92495@gmail.com>
2020-03-10 22:19:54 -0400

    Moving terraform apply to master

--- travis/script-branch.sh
+++ travis/script-branch.sh
@@ -2,11 +2,6 @@
 
 export TF_VAR_RDS_MASTER_USER_PASSWORD=$QA_RDS_MASTER_USER_PASSWORD
 
-cd terraform
-./terraform init
-./terraform apply -auto-approve
-cd ..
-
 DOMAIN="env1.qa.moonlight-health.com"
 
 ./travis/script.sh $TF_WORKSPACE $DOMAIN $TF_WORKSPACE
--- travis/script.sh
+++ travis/script.sh
@@ -4,6 +4,11 @@ ENV_NAME=$1
 DOMAIN=$2
 COGNITO_NAME=$3
 
+cd terraform
+./terraform init
+./terraform apply -auto-approve
+cd ..
+
 API_GATEWAY_REST_API_ID=$(aws apigateway get-rest-apis --query "items[?name=='${ENV_NAME}'].id" --output text)
 REACT_APP_USER_POOL_ID=$(aws cognito-idp list-user-pools --max-results 20 --query "UserPools[?Name=='${COGNITO_NAME}'].Id" --output text)
 REACT_APP_USER_CLIENT_ID=$(aws cognito-idp list-user-pool-clients --user-pool-id $REACT_APP_USER_POOL_ID --query "UserPoolClients[?ClientName=='moonlight-client'].ClientId" --output text)
