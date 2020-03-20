f6b09798bf0fb8c7d6acc4b4f752eb201eb4fe32
DESKTOP-DG4M794\Antho <anthonyv@bu.edu>
2020-03-20 15:27:39 -0400

    Took off created by for empty events

--- client/src/components/syncfusion/Scheduler.tsx
+++ client/src/components/syncfusion/Scheduler.tsx
@@ -270,9 +270,13 @@ const QuickInfoTemplatesContent: any = ({
           />
         </>
       ))}
-    <div>
-      <h3>Created by {fullName}</h3>
-    </div>
+    {!Id ? (
+      <div></div>
+    ) : (
+      <div>
+        <h3>Created by {fullName}</h3>
+      </div>
+    )}
     <div className="e-date-time">
       <div className="e-date-time-icon e-icons" />
       <div>{`${format(StartTime, "MMMM dd, yyyy")} (${format(
@@ -295,7 +299,7 @@ const QuickInfoTemplatesContent: any = ({
             new Date(Patients[p].dateOfBirth),
             "yyyy/MM/dd"
           )}`}
-          <FileInput 
+          <FileInput
             browseButtonText={"Add Patient Form..."}
             url={`patients/${p}/form`}
           />
