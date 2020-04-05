615faefe97c026176e5b331f7b7cb30ca13d2e4b
David Vargas <dvargas92495@gmail.com>
2020-04-05 17:15:47 -0400

    PR Review

--- travis/script-branch.sh
+++ travis/script-branch.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+check() {
+    exit_code=$?
+    if [ $exit_code -ne 0 ]
+    then
+        echo "Previous command failed"
+        exit $exit_code
+    fi
+}
+
 export TF_VAR_RDS_MASTER_USER_PASSWORD=$QA_RDS_MASTER_USER_PASSWORD
 
 DOMAIN="env1.qa.moonlight-health.com"
 
 ./travis/script.sh $DOMAIN
+check
 
 cd client
 npm test
-
-exit_code=$?
-if [ $exit_code -ne 0 ]
-then
-    echo "Client tests failed"
-    exit $exit_code
-fi
+check
 
 export CYPRESS_BASE_URL="https://$DOMAIN"
 cd ../qa
