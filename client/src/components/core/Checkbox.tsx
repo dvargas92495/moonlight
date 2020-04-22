48456c82ba20bee5a4307f36803894b4abe3130e
David Vargas <dvargas92495@gmail.com>
2020-04-21 20:49:10 -0400

    First Iteration of cleaning up the Scheduler

--- client/src/components/core/Checkbox.tsx
+++ client/src/components/core/Checkbox.tsx
@@ -1,13 +1,8 @@
 import React, { InputHTMLAttributes, useState, useEffect } from "react";
 import styled from "styled-components";
-import { CONTENT_COLOR } from "../../styles/colors";
 
 const StyledCheckbox = styled.input`
   cursor: pointer;
-`;
-
-const Label = styled.span`
-  color: ${CONTENT_COLOR};
 `;
 
 const Checkbox = ({
@@ -31,7 +26,7 @@ const Checkbox = ({
         onChange={(e) => setChecked(e.target.checked)}
         className={className}
       />
-      <Label>{label}</Label>
+      <span>{label}</span>
     </div>
   );
 };
