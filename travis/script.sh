baacf3a0025f744e65b08bf2ad3afdc3224726ea
David Vargas <dvargas92495@gmail.com>
2020-04-27 20:15:41 -0400

    Travis feedback 3

--- travis/script.sh
+++ travis/script.sh
@@ -15,7 +15,7 @@ fi
 cd terraform/environment
 ./terraform init
 ./terraform apply -auto-approve
-cd ..
+cd ../..
 
 if [ -z $AWS_SECRET_ACCESS_KEY ]; then
   AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
