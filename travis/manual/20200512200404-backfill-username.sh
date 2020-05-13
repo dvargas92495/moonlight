818a4ea8eeac15f0c916e2daa386e9b28279b0e5
David Vargas <dvargas92495@gmail.com>
2020-05-12 20:41:37 -0400

    Fixing query

--- travis/manual/20200512200404-backfill-username.sh
+++ travis/manual/20200512200404-backfill-username.sh
@@ -12,7 +12,7 @@ USERS=$(aws cognito-idp list-users --user-pool-id $REACT_APP_USER_POOL_ID --quer
 for u in $USERS
 do
     if [[ $u == *"@"* ]]; then
-        psql -h $RDS_MASTER_HOST -U emdeo -c "UPDATE users SET username='${u}' WHERE uuid='${UUID}'" --password $TF_VAR_RDS_MASTER_USER_PASSWORD
+        PGPASSWORD=$TF_VAR_RDS_MASTER_USER_PASSWORD psql -h $RDS_MASTER_HOST -U emdeo -c "UPDATE users SET username='${u}' WHERE uuid='${UUID}'"
     else
         UUID=$u    
     fi
