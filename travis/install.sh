bf8004c98876bd7759272546e562052532524d77
David Vargas <dvargas92495@gmail.com>
2020-06-19 20:15:40 -0400

    fixing up install script

--- travis/install.sh
+++ travis/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+
+pip install wheel
 pip install awscli --upgrade --user
 
 curl -sLo ./terraform/terraform.zip https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
@@ -9,4 +12,4 @@ echo "
 credentials \"app.terraform.io\" {
   token = \"${TERRAFORM_API_TOKEN}\"
 }
-" > /home/travis/.terraformrc
+" > $HOME/.terraformrc
