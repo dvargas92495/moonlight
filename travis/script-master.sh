122c1f1e38565410f9dc7386c51f0da8234fe7b4
David Vargas <dvargas92495@gmail.com>
2020-06-19 20:34:11 -0400

    Nuking install.sh

--- travis/script-master.sh
+++ travis/script-master.sh
@@ -4,4 +4,13 @@ set -e
 
 export TF_VAR_RDS_MASTER_USER_PASSWORD=$PROD_RDS_MASTER_USER_PASSWORD
 
+pip install awscli --upgrade --user
+curl -sLo ./terraform/terraform.zip https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
+unzip ./terraform/terraform.zip -d ./terraform/environment/
+rm ./terraform/terraform.zip
+echo "
+credentials \"app.terraform.io\" {
+  token = \"${TERRAFORM_API_TOKEN}\"
+}
+" > $HOME/.terraformrc
 ./travis/script.sh
