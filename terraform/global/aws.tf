ce841f1867c5f6a2bdeb18a9f7a37e648b2902dc
David Vargas <dvargas92495@gmail.com>
2020-06-23 11:38:52 -0400

    Bumping up to t2 medium

--- terraform/global/aws.tf
+++ terraform/global/aws.tf
@@ -129,7 +129,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name = "Emdeo-Github-Action"
 
   tags = {
