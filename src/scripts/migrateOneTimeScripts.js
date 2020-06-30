1098dff9416960206b01117daf977b5b514f0d80
David Vargas <dvargas92495@gmail.com>
2020-06-30 14:42:24 -0400

    Didn't have access to RDS_MASTER_HOST yet

--- src/scripts/migrateOneTimeScripts.js
+++ src/scripts/migrateOneTimeScripts.js
@@ -2,35 +2,47 @@ const fs = require("fs");
 const path = require("path");
 const Client = require("pg").Client;
 const spawnSync = require("child_process").spawnSync;
+const AWS = require("aws-sdk");
 
 const scripts = fs.readdirSync(path.join(__dirname, "one-time"));
-const client = new Client({
-  host: process.env.RDS_MASTER_HOST,
-  user: "emdeo",
-  password: process.env.TF_VAR_RDS_MASTER_USER_PASSWORD,
-  database: "emdeo",
-  query_timeout: 10000,
-});
-client.connect();
-client.query("SELECT * FROM one_time_scripts").then((res) => {
-  const executed = new Set(res.rows.map((r) => `${r.version}-${r.name}`));
-  return Promise.all(
-    scripts
-      .filter((s) => s.endsWith(".js"))
-      .filter((s) => !executed.has(s))
-      .map((s) => {
-        console.log(`Running one time script: ${s}`);
-        spawnSync("node", [path.join(__dirname, "one-time", s)], {
-          stdio: ["inherit", "inherit", "inherit"],
-        });
-        const version = s.substring(0, 14);
-        const name = s.substring(15);
-        return client.query(
-          "INSERT INTO one_time_scripts(version, name) VALUES ($1, $2)",
-          [version, name]
-        );
-      })
-  )
-    .then(() => client.end())
-    .then(() => console.log("Finished migrating scripts!"));
-});
+AWS.config = new AWS.Config({ region: process.env.AWS_DEFAULT_REGION });
+const rds = new AWS.RDS({ version: "2014-10-31" });
+
+rds
+  .describeDBInstances({
+    DBInstanceIdentifier: process.env.ENV_NAME,
+  })
+  .promise()
+  .then((response) => {
+    const host = response.DBInstances[0].Endpoint.Address;
+    const client = new Client({
+      host,
+      user: "emdeo",
+      password: process.env.TF_VAR_RDS_MASTER_USER_PASSWORD,
+      database: "emdeo",
+      query_timeout: 10000,
+    });
+    client.connect();
+    client.query("SELECT * FROM one_time_scripts").then((res) => {
+      const executed = new Set(res.rows.map((r) => `${r.version}-${r.name}`));
+      return Promise.all(
+        scripts
+          .filter((s) => s.endsWith(".js"))
+          .filter((s) => !executed.has(s))
+          .map((s) => {
+            console.log(`Running one time script: ${s}`);
+            spawnSync("node", [path.join(__dirname, "one-time", s)], {
+              stdio: ["inherit", "inherit", "inherit"],
+            });
+            const version = s.substring(0, 14);
+            const name = s.substring(15);
+            return client.query(
+              "INSERT INTO one_time_scripts(version, name) VALUES ($1, $2)",
+              [version, name]
+            );
+          })
+      )
+        .then(() => client.end())
+        .then(() => console.log("Finished migrating scripts!"));
+    });
+  });
